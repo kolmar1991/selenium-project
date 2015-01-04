8884da860927d81d650fb74701ad5a4409305be1
kolmar1991 <kolmar1991>
2015-01-04 17:19:21 +0100

    Renamed test classes

--- selenium-desktop/src/test/java/com.selenium.project.desktop/CreateSurveyTest.java
+++ selenium-desktop/src/test/java/com.selenium.project.desktop/CreateSurveyTest.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class Selenium1 {
+public class CreateSurveyTest {
   private WebDriver driver;
   private String baseUrl;
   private boolean acceptNextAlert = true;
--- selenium-desktop/src/test/java/com.selenium.project.desktop/FillSurveyTest.java
+++ selenium-desktop/src/test/java/com.selenium.project.desktop/FillSurveyTest.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class Selenium2 {
+public class FillSurveyTest {
   private WebDriver driver;
   private String baseUrl;
   private boolean acceptNextAlert = true;
