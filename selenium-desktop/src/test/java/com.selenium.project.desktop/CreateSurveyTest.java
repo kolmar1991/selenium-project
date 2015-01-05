324d2a5e0c0d437b2e90ee586e96049174e49461
kolmar1991 <kolmar1991>
2015-01-05 13:44:45 +0100

    Added some assertions

--- selenium-desktop/src/test/java/com.selenium.project.desktop/CreateSurveyTest.java
+++ selenium-desktop/src/test/java/com.selenium.project.desktop/CreateSurveyTest.java
@@ -1,13 +1,10 @@
-package com.selenium.project;
+package com.selenium.project.desktop;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class CreateSurveyTest {
   private WebDriver driver;
@@ -47,8 +44,10 @@ public class CreateSurveyTest {
     driver.findElement(By.name("description4")).clear();
     driver.findElement(By.name("description4")).sendKeys("produkt 4");
     driver.findElement(By.cssSelector("input.btn.btn-default")).click();
+    assertEquals("Wykluczenia", driver.findElement(By.cssSelector("div.panel-heading")).getText());
     driver.findElement(By.xpath("//td[4]/input")).click();
     driver.findElement(By.cssSelector("input.btn.btn-default")).click();
+    assertEquals("Adresaci", driver.findElement(By.cssSelector("div.panel-heading")).getText());
     driver.findElement(By.name("emails")).clear();
     driver.findElement(By.name("emails")).sendKeys("kds@o2.pl");
     driver.findElement(By.cssSelector("input.btn.btn-success")).click();
@@ -61,39 +60,6 @@ public class CreateSurveyTest {
     String verificationErrorString = verificationErrors.toString();
     if (!"".equals(verificationErrorString)) {
       fail(verificationErrorString);
-    }
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
     }
   }
 }
